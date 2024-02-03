62f26799ff972c8670ab8c70252d0caf65025328
lyubol <lyubol>
2024-02-03 17:42:42 +0000

    Fixes to Load to Raw notebook

--- DLT Freamwork/Define Metadata.sql
+++ DLT Freamwork/Define Metadata.sql
@@ -14,9 +14,9 @@ CREATE OR REPLACE TEMPORARY VIEW TempView_LatestEntityMetadata (
 ) AS
 VALUES
   -- EntityId, EntityName, EntityFormat, EntityReadOptions, EntityExpectations, EntityDescription
-  (1, 'Sales', 'csv', '{"Header": True, "inferSchema": True}', '{"ValidOrderId": "OrderId IS NOT NULL", "ValidProduct": "Product IS NOT NULL", "ValidQuantityOrdered": "QuantityOrdered IS NOT NULL AND QuantityOrdered > 0", "ValidPrice": "PriceEach IS NOT NULL AND PriceEach > 0", "ValidOrderDate": "OrderDate IS NOT NULL", "ValidPurchaseAddress": "PurchaseAddress IS NOT NULL"}', 'The Sales entity'),
-  (2, 'Product', 'csv', '{"Header": True, "inferSchema": True}', '{"ValidId": "Id IS NOT NULL"}', 'The Product entity'),
-  (3, 'Customer', 'csv', '{"Header": True, "inferSchema": True}', '{"ValidId": "Id IS NOT NULL"}', 'The Customer entity')
+  (1, 'Sales', 'csv', '{"Header": "True", "inferSchema": "True"}', '{"ValidOrderId": "OrderId IS NOT NULL", "ValidProduct": "Product IS NOT NULL", "ValidQuantityOrdered": "QuantityOrdered IS NOT NULL AND QuantityOrdered > 0", "ValidPrice": "PriceEach IS NOT NULL AND PriceEach > 0", "ValidOrderDate": "OrderDate IS NOT NULL", "ValidPurchaseAddress": "PurchaseAddress IS NOT NULL"}', 'The Sales entity'),
+  (2, 'Product', 'csv', '{"Header": "True", "inferSchema": "True"}', '{"ValidId": "Id IS NOT NULL"}', 'The Product entity'),
+  (3, 'Customer', 'csv', '{"Header": "True", "inferSchema": "True"}', '{"ValidId": "Id IS NOT NULL"}', 'The Customer entity')
 
 
 -- COMMAND ----------
